8203384b8800cad1531711f9710df97fb2c512f8
RendelFlegg <rendelflegg@ya.ru>
2022-05-24 23:13:38 +0300

    Completed Stage 4: Implemented medium level for computer player

--- Tic-Tac-Toe with AI/task/tictactoe.py
+++ Tic-Tac-Toe with AI/task/tictactoe.py
@@ -5,8 +5,17 @@ class TicTacToe:
     def __init__(self):
         self.board = {(x, y): ' ' for x in range(1, 4) for y in range(1, 4)}
         self.status = 'Game not finished'
-        self.current_move = ''
-        self.players = []
+        self.current_player = 'player_one'
+        self.players = {}
+        self.ai_dictionary = {}
+        self.lines = []
+        self.get_lines()
+
+    def drop_board(self):
+        self.board = {(x, y): ' ' for x in range(1, 4) for y in range(1, 4)}
+        self.status = 'Game not finished'
+        self.current_player = 'player_one'
+        self.players = {}
 
     def show_board(self):
         print("---------")
@@ -17,11 +26,17 @@ class TicTacToe:
             print('|', ' '.join(line), '|')
         print("---------")
 
-    def make_board(self, initial_state):
-        self.board = {(x, y): '_' for x in range(1, 4) for y in range(1, 4)}
-        for cell, initial_move in zip(self.board, initial_state):
+    def load_board(self, initial_state):
+        for cell, initial_move in zip(self.board, initial_state.replace('_', ' ')):
             self.board[cell] = initial_move
-        self.show_board()
+
+    def get_lines(self):
+        for x in range(1, 4):
+            self.lines.append([(x, y) for y in range(1, 4)])
+        for y in range(1, 4):
+            self.lines.append([(x, y) for x in range(1, 4)])
+        self.lines.append([(n, n) for n in range(1, 4)])
+        self.lines.append([(n, 4 - n) for n in range(1, 4)])
 
     def get_coordinates(self):
         while True:
@@ -40,58 +55,22 @@ class TicTacToe:
                 print('Coordinates should be from 1 to 3!')
 
     def choose_turn(self):
-        crosses = zeroes = 0
-        for value in self.board.values():
-            if value == 'X':
-                crosses += 1
-            if value == 'O':
-                zeroes += 1
-        if crosses == zeroes:
-            return 'X'
-        else:
-            return 'O'
+        return self.players[self.current_player]['mark']
 
-    def check_row(self, row, column, mark):
-        if self.board[row, column] != mark:
-            return False
+    def read_lines(self, mark, idx):
+        counter = 0
+        for coord in self.lines[idx]:
+            if self.board[coord] == mark:
+                counter += 1
+        if counter == 3:
+            return True
         else:
-            if column == 3:
-                return True
-            return self.check_row(row, column + 1, mark)
-
-    def check_rows(self, row, column, mark):
-        if row == 4:
-            return self.check_columns(row - 3, column, mark)
-            # return False
-        else:
-            if self.check_row(row, column, mark):
-                return True
-            else:
-                return self.check_rows(row + 1, column, mark)
-
-    def check_column(self, row, column, mark):
-        if self.board[row, column] != mark:
-            return False
-        else:
-            if row == 3:
-                return True
-            return self.check_column(row + 1, column, mark)
-
-    def check_columns(self, row, column, mark):
-        if column == 4:
-            return False
-        else:
-            if self.check_column(row, column, mark):
-                return True
-            else:
-                return self.check_columns(row, column + 1, mark)
-
-    def check_diagonals(self, mark):
-        return self.board[1, 1] == self.board[2, 2] == self.board[3, 3] == mark or self.board[1, 3] == self.board[
-            2, 2] == self.board[3, 1] == mark
+            if idx == 7:
+                return None
+            return self.read_lines(mark, idx + 1)
 
     def check_mark(self, mark):
-        return self.check_rows(1, 1, mark) or self.check_diagonals(mark)
+        return self.read_lines(mark, 0)
 
     def results(self):
         for mark in ['X', 'O']:
@@ -102,22 +81,19 @@ class TicTacToe:
         else:
             return 'Draw'
 
-    def random_move(self):
-        move_list = [key for key in self.board if self.board[key] == ' ']
-        print('Making move level "easy"')
-        return random.choice(move_list)
-
     def switch_player(self):
-        if self.current_move == self.players[0]:
-            self.current_move = self.players[1]
+        players_list = list(self.players.keys())
+        if self.current_player == players_list[0]:
+            self.current_player = players_list[1]
         else:
-            self.current_move = self.players[0]
+            self.current_player = players_list[0]
 
     def make_move(self):
-        if self.current_move == 'user':
+        player_type = self.players[self.current_player]['type']
+        if player_type == 'user':
             return self.get_coordinates()
         else:
-            return self.random_move()
+            return self.ai_dictionary[player_type].ai_move()
 
     def game(self):
         self.show_board()
@@ -127,11 +103,9 @@ class TicTacToe:
             self.status = self.results()
             self.show_board()
         print(self.status)
-        self.status = 'Game not finished'
-        self.board = {(x, y): ' ' for x in range(1, 4) for y in range(1, 4)}
+        self.drop_board()
 
     def get_command(self):
-        self.players = []
         while True:
             try:
                 raw_input = input('Input command: ')
@@ -139,8 +113,8 @@ class TicTacToe:
                     return raw_input
                 command, player_one, player_two = raw_input.split()
                 assert command == 'start'
-                self.players = [player_one, player_two]
-                self.current_move = player_one
+                self.players = {'player_one': {'type': player_one, 'mark': 'X'},
+                                'player_two': {'type': player_two, 'mark': 'O'}}
                 return command
             except (AssertionError, ValueError):
                 print('Bad parameters!')
@@ -152,6 +126,46 @@ class TicTacToe:
             command = self.get_command()
 
 
+class AiBasic:
+    def __init__(self, game_object):
+        self.game_object = game_object
+
+    def random_move(self):
+        move_list = [key for key in self.game_object.board if self.game_object.board[key] == ' ']
+        return random.choice(move_list)
+
+    def ai_move(self):
+        print('Making move level "easy"')
+        return self.random_move()
+
+
+class AiAdvanced(AiBasic):
+    def read_lines(self, mark, idx):
+        counter = 0
+        move = None
+        for coord in self.game_object.lines[idx]:
+            if self.game_object.board[coord] == mark:
+                counter += 1
+            if self.game_object.board[coord] == ' ':
+                move = coord
+        if counter == 2 and move:
+            return move
+        else:
+            if idx == 7:
+                return None
+            return self.read_lines(mark, idx + 1)
+
+    def ai_move(self):
+        mark = self.game_object.players[self.game_object.current_player]['mark']
+        opponent_mark = 'O' if mark == 'X' else 'X'
+        print('Making move level "medium"')
+        return self.read_lines(opponent_mark, 0) or self.read_lines(mark, 0) or self.random_move()
+
+
 if __name__ == "__main__":
     tic_tac_toe = TicTacToe()
+    alpha = AiBasic(tic_tac_toe)
+    beta = AiAdvanced(tic_tac_toe)
+    tic_tac_toe.ai_dictionary['easy'] = alpha
+    tic_tac_toe.ai_dictionary['medium'] = beta
     tic_tac_toe.menu()
