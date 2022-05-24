70d62f31cb764b542c5a20f7b173db2041d2c629
RendelFlegg <rendelflegg@ya.ru>
2022-05-24 10:37:31 +0300

    Completed Stage 2: Implemented basic game functionality

--- Tic-Tac-Toe with AI/task/tictactoe.py
+++ Tic-Tac-Toe with AI/task/tictactoe.py
@@ -1,114 +1,128 @@
-def show_board(dictionary):
-    print("---------")
-    for x in range(1, 4):
-        line = []
-        for y in range(1, 4):
-            line.append(dictionary[x, y])
-        print('|', ' '.join(line), '|')
-    print("---------")
+import random
 
 
-def move(dictionary, coordinates):
-    if dictionary[coordinates] != '_':
-        print('This cell is ocupied! Choose another one!')
+class TicTacToe:
+    def __init__(self):
+        self.board = {(x, y): ' ' for x in range(1, 4) for y in range(1, 4)}
+        self.status = 'Game not finished'
+        self.current_move = 'user'
 
+    def show_board(self):
+        print("---------")
+        for x in range(1, 4):
+            line = []
+            for y in range(1, 4):
+                line.append(self.board[x, y])
+            print('|', ' '.join(line), '|')
+        print("---------")
 
-def get_coordinates(dictionary):
-    while True:
-        try:
-            coordinates = input('Enter the coordinates: ')
-            x, y = coordinates.split()
-            x, y = int(x), int(y)
-            assert x in range(1, 4) and y in range(1, 4)
-            if dictionary[x, y] != ' ':
-                print('This cell is occupied! Choose another one!')
+    def make_board(self, initial_state):
+        self.board = {(x, y): '_' for x in range(1, 4) for y in range(1, 4)}
+        for cell, initial_move in zip(self.board, initial_state):
+            self.board[cell] = initial_move
+        self.show_board()
+
+    def get_coordinates(self):
+        while True:
+            try:
+                coordinates = input('Enter the coordinates: ')
+                x, y = coordinates.split()
+                x, y = int(x), int(y)
+                assert x in range(1, 4) and y in range(1, 4)
+                if self.board[x, y] != ' ':
+                    print('This cell is occupied! Choose another one!')
+                else:
+                    return x, y
+            except ValueError:
+                print('You should enter numbers!')
+            except AssertionError:
+                print('Coordinates should be from 1 to 3!')
+
+    def choose_turn(self):
+        crosses = zeroes = 0
+        for value in self.board.values():
+            if value == 'X':
+                crosses += 1
+            if value == 'O':
+                zeroes += 1
+        if crosses == zeroes:
+            return 'X'
+        else:
+            return 'O'
+
+    def check_row(self, row, column, mark):
+        if self.board[row, column] != mark:
+            return False
+        else:
+            if column == 3:
+                return True
+            return self.check_row(row, column + 1, mark)
+
+    def check_rows(self, row, column, mark):
+        if row == 4:
+            return self.check_columns(row - 3, column, mark)
+            # return False
+        else:
+            if self.check_row(row, column, mark):
+                return True
             else:
-                return x, y
-        except ValueError:
-            print('You should enter numbers!')
-        except AssertionError:
-            print('Coordinates should be from 1 to 3!')
+                return self.check_rows(row + 1, column, mark)
 
-
-def choose_turn(dictionary):
-    crosses = zeroes = 0
-    for value in dictionary.values():
-        if value == 'X':
-            crosses += 1
-        if value == 'O':
-            zeroes += 1
-    if crosses == zeroes:
-        return 'X'
-    else:
-        return 'O'
-
-
-def check_row(dictionary, row, column, mark):
-    if dictionary[row, column] != mark:
-        return False
-    else:
-        if column == 3:
-            return True
-        return check_row(dictionary, row, column + 1, mark)
-
-
-def check_rows(dictionary, row, column, mark):
-    if row == 4:
-        return check_columns(dictionary, row - 3, column, mark)
-        # return False
-    else:
-        if check_row(dictionary, row, column, mark):
-            return True
+    def check_column(self, row, column, mark):
+        if self.board[row, column] != mark:
+            return False
         else:
-            return check_rows(dictionary, row + 1, column, mark)
+            if row == 3:
+                return True
+            return self.check_column(row + 1, column, mark)
 
-
-def check_column(dictionary, row, column, mark):
-    if dictionary[row, column] != mark:
-        return False
-    else:
-        if row == 3:
-            return True
-        return check_column(dictionary, row + 1, column, mark)
-
-
-def check_columns(dictionary, row, column, mark):
-    if column == 4:
-        return False
-    else:
-        if check_column(dictionary, row, column, mark):
-            return True
+    def check_columns(self, row, column, mark):
+        if column == 4:
+            return False
         else:
-            return check_columns(dictionary, row, column + 1, mark)
+            if self.check_column(row, column, mark):
+                return True
+            else:
+                return self.check_columns(row, column + 1, mark)
+
+    def check_diagonals(self, mark):
+        return self.board[1, 1] == self.board[2, 2] == self.board[3, 3] == mark or self.board[1, 3] == self.board[
+            2, 2] == self.board[3, 1] == mark
+
+    def check_mark(self, mark):
+        return self.check_rows(1, 1, mark) or self.check_diagonals(mark)
+
+    def results(self):
+        for mark in ['X', 'O']:
+            if self.check_mark(mark):
+                return f'{mark} wins'
+        if ' ' in self.board.values():
+            return 'Game not finished'
+        else:
+            return 'Draw'
+
+    def random_move(self):
+        move_list = [key for key in self.board if self.board[key] == ' ']
+        print('Making move level "easy"')
+        return random.choice(move_list)
+
+    def make_move(self):
+        if self.current_move == 'user':
+            self.current_move = 'computer'
+            return self.get_coordinates()
+        else:
+            self.current_move = 'user'
+            return self.random_move()
+
+    def game(self):
+        self.show_board()
+        while self.status == 'Game not finished':
+            self.board[self.make_move()] = self.choose_turn()
+            self.status = self.results()
+            self.show_board()
+        print(self.status)
 
 
-def check_diagonals(dictionary, mark):
-    return dictionary[1, 1] == dictionary[2, 2] == dictionary[3, 3] == mark or dictionary[1, 3] == dictionary[2, 2] == dictionary[3, 1] == mark
-
-
-def check_mark(dictionary, mark):
-    return check_rows(dictionary, 1, 1, mark) or check_diagonals(dictionary, mark)
-
-
-def results(dictionary):
-    for mark in ['X', 'O']:
-        if check_mark(dictionary, mark):
-            return f'{mark} wins'
-    if ' ' in dictionary.values():
-        return 'Game not finished'
-    else:
-        return 'Draw'
-
-
-dict_keys = [(1, 1), (1, 2), (1, 3), (2, 1), (2, 2), (2, 3), (3, 1), (3, 2), (3, 3)]
-user_input = input('Enter the cells: ').replace('_', ' ')
-
-board_dict = dict(zip(dict_keys, user_input))
-
-show_board(board_dict)
-
-coordinates = get_coordinates(board_dict)
-board_dict[coordinates] = choose_turn(board_dict)
-
-show_board(board_dict)
-print(results(board_dict))
+if __name__ == "__main__":
+    tic_tac_toe = TicTacToe()
+    tic_tac_toe.game()
